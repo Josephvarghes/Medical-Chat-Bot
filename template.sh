0860ee0642777cbde73376851b22f9d49f817258
Joseph Varghese <josephvarghese98128@gmail.com>
2025-08-14 16:49:24 +0530

    created project setup

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating directory
+mkdir -p src 
+mkdir -p research 
+
+#creating the files 
+touch src/__init__.py 
+touch src/helper.py 
+touch src/prompt.py 
+touch .env 
+touch steup.py 
+touch app.py 
+touch research/trials.ipynb 
+touch requirements.tx 
+
+echo " directory and files created successfully
