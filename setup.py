731776c6ae2e247533bbfc41390ceebd91ca5f60
Joseph Varghese <josephvarghese98128@gmail.com>
2025-08-16 12:10:49 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="Medical_Chatbot", 
+    version="0.1.0", 
+    author="Joseph Varghese", 
+    author_email="josephvarghese98128@gmail.com", 
+    packages=find_packages(), 
+    install_requires = [] 
+) 
+#1:09:59 
